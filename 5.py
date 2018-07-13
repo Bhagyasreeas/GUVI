52a020932edc5cf6c70d39598e36057f257a7bc8
Bhagyasreeas <40803702+Bhagyasreeas@users.noreply.github.com>
2018-07-13 11:00:05 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,15 @@
+x=input()
+y=input()
+z=input()
+if x.isalpha() or y.isalpha() or z.isalpha():
+    print("invalid")
+else:
+    x=int(x)
+    y=int(y)
+    z=int(z)
+    if x>=y and x>=z:
+        print(x)
+    elif y>=x and y>=z:
+        print(y)
+    elif z>=x and z>=y:
+        print(z)
